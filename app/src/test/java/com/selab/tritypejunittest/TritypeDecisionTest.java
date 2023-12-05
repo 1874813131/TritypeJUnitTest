0a9daadd07536ecf14d650b629f1e940650b4ee5
jiangxiansheng <1874813131@qq.com>
2023-12-05 23:57:36 +0800

    assignment2

---
+++ app/src/test/java/com/selab/tritypejunittest/TritypeDecisionTest.java
@@ -0,0 +1,17 @@
+package com.selab.tritypejunittest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TritypeDecisionTest {
+
+    @Test
+    public void testTriang() {
+        //两边之和是否小于等于第三边
+        Tritype tri = new Tritype();
+        assertEquals(4, tri.Triang(1, 2, 7));
+        assertEquals(1, tri.Triang(2, 3, 4));
+    }
+
+}
