4bdb1c2009b842b3dfc0651e104719ea3c45e368
jiangxiansheng <1874813131@qq.com>
2023-12-06 00:00:30 +0800

    assignment2

--- app/src/test/java/com/selab/tritypejunittest/TritypeMCDCTest.java
+++ app/src/test/java/com/selab/tritypejunittest/TritypeMCDCTest.java
@@ -10,8 +10,10 @@ public class TritypeMCDCTest {
     public void testTriang() {
         //两边之和是否小于等于第三边
         Tritype tri = new Tritype();
-        assertEquals(4, tri.Triang(1, 2, 7));
-        assertEquals(1, tri.Triang(1, 2, 2));
+        assertEquals(3, tri.Triang(1, 1, 1));
+        assertEquals(4, tri.Triang(0, 1, 1));
+        assertEquals(4, tri.Triang(1, 0, 1));
+        assertEquals(4, tri.Triang(1, 1, 0));
     }
 
 }
